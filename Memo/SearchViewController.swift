3c8ce654b6324729730480fc5962b4fc25347ef1
hailuy <hailuy@users.noreply.github.com>
2016-07-05 14:12:52 +0800

    clean text

--- Memo/SearchViewController.swift
+++ Memo/SearchViewController.swift
@@ -12,7 +12,6 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
     
     let cellIdentifier:String = "ItemCell"
     var mainTableView:UITableView = UITableView()
-    //    private var originConstant: CGFloat = 0
     var dataArr:[ItemModel] = [ItemModel]()
     var dataArr1:[ItemModel] = [ItemModel]()
     var isFinished:Bool!    //表示当前标签页是"已完成"还是"未完成"。
@@ -23,22 +22,13 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
     
     var filtered:[ItemModel] = [ItemModel]()
     var filtered1:[ItemModel] = [ItemModel]()
-    //    var editVC = EditController()
-    //    var resultSearchController : UISearchController = UISearchController()
     lazy var searchBar:UISearchBar = UISearchBar(frame: CGRectMake(0, 0, 200, 44))
-    //    lazy var search:UITextField = UITextField(frame: CGRectMake(0, 0, 200, 20))
-    //    var searchController:UISearchController!
     var tap:UITapGestureRecognizer!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tap = UITapGestureRecognizer(target: self, action: "clicked")
-
-        //        searchController = UISearchController(searchResultsController: nil)
         searchBar.delegate = self
-        //        searchController.searchResultsUpdater = self
-        //        searchController.dimsBackgroundDuringPresentation = false
-        //        definesPresentationContext = true
         mainTableView.delegate = self
         mainTableView.dataSource = self
         
@@ -59,67 +49,19 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
         self.mainTableView.backgroundColor = UIColor.clearColor()
         self.mainTableView.separatorStyle = .None
         
-        
-        //        self.dataBase.open()
-        //
-        //        self.dataBase.close()
-        
         let resetButton = UIBarButtonItem(title: "返回", style: UIBarButtonItemStyle.Plain, target: self, action: Selector("reset"))
-        //        self.resultSearchController = ({
-        //            let controller = UISearchController(searchResultsController: nil)
-        //            controller.searchResultsUpdater = self
-        ////            controller.
-        //            controller.dimsBackgroundDuringPresentation = false
-        //            controller.searchBar.sizeToFit()
-        //
-        //            self.mainTableView.tableHeaderView = controller.searchBar
-        //
-        //            return controller
-        //        })()
-        //        searchController.searchBar = UISearchBar()
         searchBar.searchBarStyle = UISearchBarStyle.Minimal
         
         self.dataArr.appendContentsOf(DatabaseService.sharedInstance.selectAllInDB().0)
         self.dataArr1.appendContentsOf(DatabaseService.sharedInstance.selectAllInDB().1)
-        //print("kjkfdgsdfghjk",dataArr1)
-        //        print(self.dataArr)
-        //        onInput()
-        //        var searchBarStyle: UISearchBarStyle = .Minimal
-        //        searchBar.sizeToFit().addTarget(self,action:"onInput",forControlEvents:)
-        //        search.addTarget(self, action: "onInput", forControlEvents: .AllEditingEvents)
-        //        search.backgroundColor = UIColor.clearColor()
         navigationItem.titleView = searchBar
-        //        self.mainTableView.tableHeaderView = self.searchBar
         self.navigationItem.leftBarButtonItem = resetButton
         
         // Reload the table
         self.mainTableView.reloadData()
     }
     
-    //    func onInput() {
-    //        //            tabledata.append(i.title)
-    ////            tabledata.append(i.title)
-    //
-    //        var index = 0
-    //        self.dataArr = FinishedVC.selectAllInDB().0
-    //        for i in self.dataArr {
-    ////            if i.title == searchBar.text || i.content == searchBar.text {
-    ////            tabledata[index]
-    //            index++
-    ////            }
-    //        }
-    //        self.dataArr.appendContentsOf(FinishedVC.selectAllInDB().1)
-    //        for i in self.dataArr {
-    ////            if i.title == searchBar.text || i.content == searchBar.text {
-    //                tabledata[index] = i.title
-    //                index++
-    ////            }
-    //        }
-    //    }
     
-   // override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-    //    self.view.endEditing(true)
-  //  }
     func clicked(){
         self.searchBar.resignFirstResponder()
     }
@@ -143,20 +85,9 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
-        
-        //        filtered = dataArr.filter({ (text) -> Bool in
-        //            let tmp: NSString = text
-        //            let range = tmp.rangeOfString(searchText, options: NSStringCompareOptions.CaseInsensitiveSearch)
-        //            return range.location != NSNotFound
-        //        })
-        
-        //        self.updateSearchResultsForSearchController(self.searchBar)
-        
         let searchText = searchBar.text
-        
         filterContentForSearchText(searchText!)
         filterContentForSearchText1(searchText!)
-//        print(searchText)
         
         if(filtered.count == 0 && filtered1.count == 0){
             searchActive = false;
@@ -165,6 +96,7 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
         }
         self.mainTableView.reloadData()
     }
+    
     // 告诉tableview一共有多少组
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 2
@@ -200,16 +132,9 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func reset() {
-        //        let nav = BaseViewController()
         self.navigationController!.popViewControllerAnimated(true)
     }
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //        if self.searchBar.active {
-        //            return self.filteredTableData.count
-        //        }else{
-        //        return self.tabledata.count
-        //        }
-        //        if(searchActive) {
         if (filtered.count != 0 || filtered1.count != 0) {
             if section == 0 {
                 return filtered.count
@@ -219,33 +144,9 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
             }
         }
         return 0
-        
-        //        }
-        //        return self.dataArr.count
     }
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        //        var section = indexPath.section
-        //        var row = indexPath.row
-        //        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier:"ItemCell")
-        //        cell.selectionStyle =  UITableViewCellSelectionStyle.None
-        //        cell.backgroundColor = UIColor.clearColor()
-        //        cell.contentView.backgroundColor = UIColor.clearColor()
-        //        cell.textLabel?.textAlignment = NSTextAlignment.Left
-        //        cell.textLabel?.textColor = UIColor.blackColor()
-        //        cell.textLabel?.font = UIFont.systemFontOfSize(14.0)
-        //
-        //        //        if self.searchBar.active {
-        //        //            cell.textLabel?.text = filteredTableData[indexPath.row]
-        //        //        } else {
-        //        cell.textLabel?.text = tabledata[indexPath.row]
-        //        //        }
-        //        return cell
-        //
-        
         let cell = mainTableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! ItemCell
-        
-//        let i = cell.viewWithTag(_ tag: Int)
-        
         if(searchActive){
             
             if indexPath.section == 0 {
@@ -261,14 +162,7 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
                 cell.selectionStyle = UITableViewCellSelectionStyle.None
             }
         }
-//        else {
-//            let item = self.dataArr[indexPath.row]
-//            cell.titleLabel.text = item.title
-//            cell.timeLabel.text = friendlyTime(item.lastEditTime)
-//            cell.selectionStyle = UITableViewCellSelectionStyle.None
-//        }
         return cell
-        //        return FinishedVC.tableView(tableView, cellForRowAtIndexPath: indexPath)
     }
     
     func filterContentForSearchText(searchText: String) {
@@ -293,20 +187,7 @@ class SearchViewController: UIViewController, UITableViewDataSource, UITableView
             
         })
     }
-//
-//    func updateSearchResultsForSearchController(searchBar: UISearchBar) {
-//        let searchText = searchBar.text
-//        
-//        filterContentForSearchText(searchText!)
-//        //        filtered.removeAll(keepCapacity: false)
-//        //        let searchPredicate = NSPredicate(format: "SELF CONTAINS[c] %@", searchController.searchBar.text!)
-//        //
-//        //        let array = (dataArr as NSArray).filteredArrayUsingPredicate(searchPredicate)
-//        //        filtered = array as! [ItemModel]
-//        self.mainTableView.reloadData()
-//        
-//    }
-//    
+    
     //显示友好时间戳，参考自http://blog.csdn.net/zhyl8157121/article/details/42155921
     func friendlyTime(dateTime: String) -> String {
         let dateFormatter = NSDateFormatter()
